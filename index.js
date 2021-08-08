883b431934ff09a98a1776508135ddc649cf18d5
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-08-07 22:06:05 -0400

    got server running on port 7777

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it here
+
+const server = require('./api/server');
+
+const port = 7777
+
+server.listen(7777, () => {
+    console.log(`server online at ${port}`)
+})
